1321517195e1713b1d36bcf705f91f4790cdcc9b
Gabriel Silva Barros <08175460148@mma.gov.br>
2024-02-23 17:33:44 -0300

    feat: create node anywhere in the canvas

--- src/components/MenuBar.tsx
+++ src/components/MenuBar.tsx
@@ -10,17 +10,39 @@ import {
     MenubarTrigger,
 } from "@/components/ui/menubar"
 import useNodeStore from "@/store/NodeStore"
+import { useEffect, useState } from "react";
 
 
 const MenuBar = () => {
-
+    const [isCreatingNode, setIsCreatingNode] = useState(false);
     const addNode = useNodeStore((state) => state.addNode)
+
+    const handleCanvasClick = (event) => {
+        if (isCreatingNode) {
+            const { offsetX, offsetY } = event;
+            addNode({
+                id: crypto.randomUUID(),
+                position: { x: offsetX, y: offsetY },
+                data: { label: "NEW" },
+                type: "square",
+            });
+            setIsCreatingNode(false); // Disable node creation mode after placing the node
+        }
+    };
+
+    // Add an event listener for canvas click events
+    useEffect(() => {
+        document.addEventListener('click', handleCanvasClick);
+        return () => {
+            document.removeEventListener('click', handleCanvasClick);
+        };
+    }, [isCreatingNode]);
 
     const addSquareNode = () => {
         addNode({
             id: crypto.randomUUID(),
             position: { x: 500, y: 800 },
-            data: {label: "NOVO"},
+            data: { label: "NOVO" },
             type: "square",
         },)
     }
@@ -28,10 +50,12 @@ const MenuBar = () => {
     return (
         <Menubar className='fixed bottom-20 left-1/2 -translate-x-1/2 bg-white rounded-2xl shadow-lg border-zinc-300 px-8 h-28 w-96 overflow-hidden'>
             <MenubarMenu>
-                <MenubarTrigger onClick={addSquareNode} className="w-28 h-28 bg-emerald-500 mt-6 rounded  transition-transform cursor-pointer hover:-translate-y-2"></MenubarTrigger>
+                <MenubarTrigger onClick={() => setIsCreatingNode(true)} className={`${isCreatingNode ? "cursor-crosshair" : ""} w-28 h-28 bg-emerald-500 mt-6 rounded  transition-transform cursor-pointer hover:-translate-y-2`}></MenubarTrigger>
             </MenubarMenu>
         </Menubar>
     )
 }
+
+
 
 export default MenuBar
