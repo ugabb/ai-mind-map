b82dfbb4c00aada8949cdd66d35945093243b261
Gabriel Silva Barros <08175460148@mma.gov.br>
2024-03-05 15:26:09 -0300

    feat: creating close nodes by direction. - installed react-icons - create a object to manipulate state for each direction

--- src/components/Square.tsx
+++ src/components/Square.tsx
@@ -4,18 +4,33 @@ import useNodeStore from "@/store/NodeStore";
 import { useCallback, useEffect, useRef, useState } from "react";
 import { Handle, NodeProps, NodeResizer, Position, useNodeId, useNodesState } from "reactflow";
 
+import { PiArrowCircleDownThin, PiArrowCircleLeftThin, PiArrowCircleRightThin, PiArrowCircleUpThin } from "react-icons/pi";
+
 import 'reactflow/dist/style.css';
 
-import { blue } from "tailwindcss/colors";
+import { blue, red } from "tailwindcss/colors";
 
+interface IDirection {
+    top: boolean;
+    bottom: boolean;
+    left: boolean;
+    right: boolean;
+}
 
 const Square = ({ selected, data, id, xPos, yPos }: NodeProps) => {
     const inputRef = useRef<HTMLInputElement>(null);
     const nodes = useNodeStore((state) => state.nodes)
     const [isEditing, setIsEditing] = useState(true);
+    const [isAddingNode, setIsAddingNode] = useState<IDirection>({
+        top: false,
+        bottom: false,
+        left: false,
+        right: false
+    });
     const [editedLabel, setEditedLabel] = useState(data.label);
 
     const updateNodeText = useNodeStore((state) => state.updateNodeText)
+    const addNode = useNodeStore((state) => state.addNode)
     const deleteNode = useNodeStore((state) => state.deleteNode)
 
     const handleDoubleClick = () => {
@@ -60,7 +75,7 @@ const Square = ({ selected, data, id, xPos, yPos }: NodeProps) => {
     }, [handleKeyDown])
 
     return (
-        <div id={id} className="flex justify-center items-center font-medium  bg-emerald-300 rounded w-full h-full min-w-[200px] min-h-[200px]" onDoubleClick={handleDoubleClick} onDragEnd={() => console.log("dropped")}>
+        <div id={id} className="flex justify-center items-center font-medium relative  bg-emerald-300 rounded w-full h-full min-w-[200px] min-h-[200px]" onDoubleClick={handleDoubleClick} onDragEnd={() => console.log("dropped")}>
             {isEditing ? (
                 <input
                     ref={inputRef}
@@ -70,7 +85,7 @@ const Square = ({ selected, data, id, xPos, yPos }: NodeProps) => {
                     onChange={handleInputChange}
                     onBlur={handleInputBlur}
                     autoFocus
-                    className="bg-transparent border-none outline-none  cursor-text text-center placeholder:text-gray-700"
+                    className="bg-transparent border-none outline-none  cursor-text text-center placeholder:text-gray-700/80"
                 />
             ) : (
                 <>
@@ -83,31 +98,209 @@ const Square = ({ selected, data, id, xPos, yPos }: NodeProps) => {
                     />
 
 
-                    <Handle
-                        id="right"
-                        type="source"
-                        position={Position.Right}
-                        style={{ background: blue[400], width: "12px", height: "12px", right: "-20px" }}
-                    />
+                    {isAddingNode.right
+                        ?
+                        (
+                            <Handle
+                                id="right"
+                                type="source"
+                                position={Position.Right}
 
-                    <Handle
-                        id="left"
-                        type="source"
-                        position={Position.Left}
-                        style={{ background: blue[400], width: "12px", height: "12px", left: "-20px" }}
-                    />
-                    <Handle
-                        id="top"
-                        type="source"
-                        position={Position.Top}
-                        style={{ background: blue[400], width: "12px", height: "12px", top: "-20px" }}
-                    />
-                    <Handle
-                        id="bottom"
-                        type="source"
-                        position={Position.Bottom}
-                        style={{ background: blue[400], width: "12px", height: "12px", bottom: "-20px" }}
-                    />
+                                onMouseLeave={() => setIsAddingNode(prev => ({
+                                    ...prev,
+                                    right: false
+                                }))}
+                                onClick={() => addNode({
+                                    id: crypto.randomUUID(),
+                                    position: { x: xPos + 300, y: yPos },
+                                    data: { label: "" },
+                                    type: "square",
+                                })}
+                                className="flex items-center justify-center bg-transparent size-10 -right-10 border-none"
+                            >
+                                <PiArrowCircleRightThin className="size-12 text-blue-400" />
+                            </Handle>
+                        )
+                        :
+                        (
+                            <Handle
+                                id="right"
+                                type="source"
+                                position={Position.Right}
+                                style={{ background: blue[400], width: "12px", height: "12px", right: "-20px" }}
+                                onMouseEnter={() => setIsAddingNode(prev => ({
+                                    ...prev,
+                                    right: true
+                                }))}
+                            />
+                        )
+                    }
+
+                    {isAddingNode.right && <div
+                        className='bg-emerald-400/20 rounded  min-w-[200px] min-h-[200px]'
+                        style={{
+                            position: 'absolute',
+                            left: 300, // Adjust the offset as needed
+                            // top: 5, // Adjust the offset as neededty
+                            pointerEvents: 'none', // Allow clicks to pass through
+                            zIndex: 1, // Ensure it's above the background
+                        }}
+                    />}
+
+
+                    {isAddingNode.left
+                        ?
+                        (
+                            <Handle
+                                id="left"
+                                type="source"
+                                position={Position.Left}
+                                onMouseLeave={() => setIsAddingNode((prev) => ({
+                                    ...prev,
+                                    left: false
+                                }))}
+                                onClick={() => addNode({
+                                    id: crypto.randomUUID(),
+                                    position: { x: xPos - 300, y: yPos },
+                                    data: { label: "" },
+                                    type: "square",
+                                })}
+                                className="flex items-center justify-center bg-transparent size-10 -left-10 border-none"
+                            >
+                                <PiArrowCircleLeftThin className="size-12 text-blue-400" />
+                            </Handle>
+                        )
+                        :
+                        (
+                            <Handle
+                                id="left"
+                                type="source"
+                                position={Position.Left}
+                                style={{ background: blue[400], width: "12px", height: "12px", left: "-20px" }}
+                                onMouseEnter={() => setIsAddingNode(
+                                    (prev) => ({
+                                        ...prev,
+                                        left: true
+                                    })
+                                )}
+                            />
+                        )
+                    }
+
+
+                    {isAddingNode.left && <div
+                        className='bg-emerald-400/20 rounded  min-w-[200px] min-h-[200px]'
+                        style={{
+                            position: 'absolute',
+                            left: -300, // Adjust the offset as needed
+                            // top: 5, // Adjust the offset as neededty
+                            pointerEvents: 'none', // Allow clicks to pass through
+                            zIndex: 1, // Ensure it's above the background
+                        }}
+                    />}
+
+
+                    {isAddingNode.top
+                        ?
+                        (
+                            <Handle
+                                id="top"
+                                type="source"
+                                position={Position.Top}
+                                onMouseLeave={() => setIsAddingNode((prev) => ({
+                                    ...prev,
+                                    top: false
+                                }))}
+                                onClick={() => addNode({
+                                    id: crypto.randomUUID(),
+                                    position: { x: xPos, y: yPos - 300 },
+                                    data: { label: "" },
+                                    type: "square",
+                                })}
+                                className="flex items-center justify-center bg-transparent size-10 -top-10 border-none"
+                            >
+                                <PiArrowCircleUpThin className="size-12 text-blue-400" />
+                            </Handle>
+                        )
+                        :
+                        (
+                            <Handle
+                                id="top"
+                                type="source"
+                                position={Position.Top}
+                                style={{ background: blue[400], width: "12px", height: "12px", top: "-20px" }}
+                                onMouseEnter={() => setIsAddingNode(
+                                    (prev) => ({
+                                        ...prev,
+                                        top: true
+                                    })
+                                )}
+                            />
+                        )
+                    }
+
+                    {isAddingNode.top && <div
+                        className='bg-emerald-400/20 rounded  min-w-[200px] min-h-[200px]'
+                        style={{
+                            position: 'absolute',
+                            // left: -300, // Adjust the offset as needed
+                            top: -300, // Adjust the offset as neededty
+                            pointerEvents: 'none', // Allow clicks to pass through
+                            zIndex: 1, // Ensure it's above the background
+                        }}
+                    />}
+
+
+                    {isAddingNode.bottom
+                        ?
+                        (
+                            <Handle
+                                id="bottom"
+                                type="source"
+                                position={Position.Bottom}
+                                onMouseLeave={() => setIsAddingNode((prev) => ({
+                                    ...prev,
+                                    bottom: false
+                                }))}
+                                onClick={() => addNode({
+                                    id: crypto.randomUUID(),
+                                    position: { x: xPos, y: yPos + 300 },
+                                    data: { label: "" },
+                                    type: "square",
+                                })}
+                                className="flex items-center justify-center bg-transparent size-10 -bottom-10 border-none"
+                            >
+                                <PiArrowCircleDownThin className="size-12 text-blue-400" />
+                            </Handle>
+                        )
+                        :
+                        (
+                            <Handle
+                                id="bottom"
+                                type="source"
+                                position={Position.Bottom}
+                                style={{ background: blue[400], width: "12px", height: "12px", bottom: "-20px" }}
+                                onMouseEnter={() => setIsAddingNode(
+                                    (prev) => ({
+                                        ...prev,
+                                        bottom: true
+                                    })
+                                )}
+                            />
+                        )
+                    }
+
+                    {isAddingNode.bottom && <div
+                        className='bg-emerald-400/20 rounded  min-w-[200px] min-h-[200px]'
+                        style={{
+                            position: 'absolute',
+                            // left: -300, // Adjust the offset as needed
+                            top: 300, // Adjust the offset as neededty
+                            pointerEvents: 'none', // Allow clicks to pass through
+                            zIndex: 1, // Ensure it's above the background
+                        }}
+                    />}
+
 
 
                     {/* ... (other handles) */}
